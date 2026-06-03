14195f405eec789c1c1eda9c146dcdf54848ff55
Dmitrii Shostak <dmitshostak@gmail.com>
2026-06-03 20:38:59 +0900

    Practice

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,7 +1,7 @@
 const sumAll = function(s, e) {
     if(!(Number.isInteger(s) && Number.isInteger(e))) return "ERROR"
     if(s < 0 || e < 0) return "ERROR"
-    if(s > e) [s, e] = [e, s]
+    if(s > e) [s, e] = [e, s];
     let r = 0
     for(let i = s; i <= e; i++) {
         r += i
